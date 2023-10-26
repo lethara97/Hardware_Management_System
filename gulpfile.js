6c4539e890f7dd4a568b89e73e715fde354d3dec
Keshara1997 <kavindakeshara97@gmail.com>
2023-10-26 11:02:57 +0530

    add scss file and packages

---
+++ gulpfile.js
@@ -0,0 +1,14 @@
+const {src,dest,watch,series}=require('gulp')
+const sass=require('gulp-sass')(require('sass'))
+
+function buildStyles(){
+    return src('sass/**/*.scss')
+    .pipe(sass())
+    .pipe(dest('css'))
+}
+
+function watchTasks(){
+    watch(['sass/**/*.scss'],buildStyles)
+}
+
+exports.default=series(buildStyles,watchTasks)
