421dab5caedcae84a9c9b78676f1bd320d4ca702
Keshara1997 <kavindakeshara97@gmail.com>
2024-03-12 20:10:17 +0530

    hasitha pamal

--- src/components/Delivery/Delivery-Create.component.js
+++ src/components/Delivery/Delivery-Create.component.js
@@ -212,3 +212,407 @@ export default class CreateDelivery extends Component {
         );
     }
 }
+
+
+
+{/* /*
+ * Copyright (C) 2023 Evoke Labs - All Rights Reserved
+ */
+
+'use client'
+
+import React, { useCallback, useEffect, useMemo, useState } from 'react'
+
+import moment from 'moment-timezone'
+import { useRouter, useSearchParams } from 'next/navigation'
+
+import ButtonComponent from '@components/button.component'
+import ComboBoxComponent from '@components/comboBox.component'
+import DatePickerComponent from '@components/datePicker.component'
+import FormElementComponent from '@components/formElement.component'
+import InputComponent from '@components/input.component'
+import CardLayout from '@components/layouts/card.layout'
+import PaginationComponent from '@components/pagination.component'
+import TableComponent from '@components/table.component'
+import { AlertType, useMessageCtx } from '@contexts/message.context'
+import {
+  buildRoute,
+  currencyFormat,
+  fineractDateToMoment
+} from '@helpers/global'
+import { GlaccountsGetResponse } from '@modals/GlaccountsGetResponse'
+import { OfficesGetResponse } from '@modals/OfficesGetResponse'
+import { useGlaccounts } from '@services/glaccounts'
+import { useSearchJournalEntries } from '@services/journalentries'
+import { useOffices } from '@services/offices.c'
+
+export type SearchJournalEntries = {
+  dateFormat?: string
+  fromDate?: string
+  glAccountId?: number
+  limit?: number
+  locale?: string
+  manualEntriesOnly?: number
+  officeId?: number
+  offset?: number
+  toDate?: string
+  transactionId?: string
+}
+function SearchJournalEntriesPage() {
+  const searchParams = useSearchParams()
+  const glAccountId = parseInt(searchParams?.get('glAccountId')!, 10)
+  const officeId = searchParams?.get('officeId')!
+  const manualEntriesOnly = searchParams?.get('manualEntriesOnly')!
+  const fromDate = searchParams?.get('fromDate')!
+  const toDate = searchParams?.get('toDate')!
+  const transactionId = searchParams?.get('transactionId')!
+  const offset = searchParams?.get('offset')!
+  const limit = searchParams?.get('limit')!
+
+  const [data, setData] = useState<SearchJournalEntries>({
+    limit: 10,
+    offset: 0
+  })
+
+  const { data: responseData, loading } = useSearchJournalEntries({
+    glAccountId,
+    officeId: parseInt(officeId, 10),
+    manualEntriesOnly: parseInt(manualEntriesOnly, 10),
+    fromDate: fromDate ? moment(fromDate).format('DD MMMM YYYY') : undefined,
+    toDate: toDate ? moment(toDate).format('DD MMMM YYYY') : undefined,
+    transactionId,
+    offset: parseInt(offset, 10),
+    limit: parseInt(limit, 10)
+  })
+
+  const {
+    data: offices,
+    error: officesError,
+    loading: officesLoading
+    // mutate: officesMutate
+  } = useOffices()
+
+  const {
+    data: glaccountsData,
+    error: glaccountsError,
+    loading: glaccountsLoading
+    // mutate: glaccountsMutate
+  } = useGlaccounts({ disabled: false, manualEntriesAllowed: true, usage: 1 })
+
+  const keyValuePairs = useMemo<{ key: number; value: string }[]>(
+    () => [
+      { key: 0, value: 'All' },
+      { key: 1, value: 'Manual' },
+      { key: 2, value: 'System' }
+    ],
+    []
+  )
+
+  const router = useRouter()
+
+  const selectedOffice = useMemo(
+    () => offices?.find?.((o) => o.id === data?.officeId),
+    [data?.officeId, offices]
+  )
+
+  const selectedFilter = useMemo(
+    () => keyValuePairs?.find?.((o) => o.key === data?.manualEntriesOnly),
+    [data?.manualEntriesOnly, keyValuePairs]
+  )
+
+  const selectedAddDbit = useMemo(
+    () => glaccountsData?.find?.((o) => o.id === data?.glAccountId),
+    [data, glaccountsData]
+  )
+
+  const { pushAlert } = useMessageCtx()
+
+  const formattedData = useMemo(
+    () =>
+      responseData?.pageItems?.map((c) => [
+        // c.displayName?.toString() ?? '',
+        c.id?.toString() ?? '',
+        c.officeName ?? '',
+        fineractDateToMoment(c.transactionDate)
+          ?.format('DD MMMM yyyy')
+          .toString() ?? '',
+        c.transactionId ?? '',
+        c.glAccountType?.value ?? '',
+        c.createdByUserName ?? '',
+        `${c.glAccountName}(${c.glAccountId})` ?? '',
+        c.entryType?.value === 'DEBIT' ? currencyFormat(c.amount!) : '',
+        c.entryType?.value === 'CREDIT' ? currencyFormat(c.amount!) : ''
+      ]) ?? [],
+    [responseData]
+  )
+
+  const updateQueryParams = useCallback(
+    (d?: SearchJournalEntries) => {
+      const queryParams = new URLSearchParams(
+        Object.fromEntries(
+          Object.entries(d ?? data).filter(([_key, value]) => value !== null)
+        ) as any
+      )
+      router.push(
+        buildRoute(`accounting/searchtransaction?${queryParams.toString()}`)
+      )
+    },
+    [data, router]
+  )
+
+  const getDefaultOption = (def: any, options: any) => {
+    return options ? [def, ...options] : []
+  }
+
+  const search = useCallback(async () => {
+    try {
+      if (!data?.glAccountId && !data?.transactionId) {
+        return pushAlert({
+          message: 'Need to select Account Name or Code',
+          type: AlertType.Error
+        })
+      }
+      updateQueryParams()
+    } catch (e: any) {
+      if (e?.response?.data?.defaultUserMessage)
+        return pushAlert({
+          message: e?.response?.data?.defaultUserMessage,
+          type: AlertType.Error
+        })
+      if (e?.message)
+        return pushAlert({ message: e?.message, type: AlertType.Error })
+    }
+  }, [data?.glAccountId, data?.transactionId, pushAlert, updateQueryParams])
+
+  useEffect(() => {
+    setData?.((cs) => ({
+      ...cs,
+      glAccountId,
+      transactionId: transactionId!,
+      toDate: toDate!,
+      fromDate: fromDate!,
+      manualEntriesOnly: parseInt(manualEntriesOnly!, 10),
+      officeId: parseInt(officeId!, 10)
+    }))
+  }, [
+    glAccountId,
+    transactionId,
+    toDate,
+    fromDate,
+    manualEntriesOnly,
+    officeId
+  ])
+  return (
+    <div className="mx-auto max-w-7xl px-4 sm:px-6 md:px-8">
+      <CardLayout>
+        <div className="flex justify-between">
+          <div className="px-4 py-5 sm:px-6">
+            <h3 className="text-lg font-medium leading-6 text-gray-900">
+              Search Journal Entries
+            </h3>
+          </div>
+        </div>
+        <div className="border-t border-gray-200 " />
+        <div className="border-t border-gray-200 px-4 py-5 sm:px-6">
+          <dl className="grid grid-cols-4 gap-x-4 gap-y-4 ">
+            <FormElementComponent
+              label="Select Account name or Code"
+              loading={glaccountsLoading}
+              error={glaccountsError}
+            >
+              <ComboBoxComponent
+                value={selectedAddDbit}
+                options={glaccountsData}
+                optionLabel={(i) => (i.name ? `${i.name} ${i.glCode}` : '')}
+                optionKey={(i) => i.id ?? ''}
+                placeholder="-Select Account name or Code-"
+                onChange={(item) => {
+                  setData?.((cs) => ({
+                    ...cs,
+                    glAccountId: (item as GlaccountsGetResponse).id
+                  }))
+                }}
+                filterItem={(item, query) =>
+                  (item?.name
+                    ?.toLowerCase?.()
+                    ?.includes?.(query.toLowerCase()) ||
+                    item?.glCode?.includes?.(query)) ??
+                  true
+                }
+              />
+            </FormElementComponent>
+            <FormElementComponent
+              label="Office"
+              loading={officesLoading}
+              error={officesError}
+            >
+              <ComboBoxComponent
+                value={selectedOffice}
+                options={offices}
+                optionLabel={(i) => i.name ?? ''}
+                optionKey={(i) => i.id ?? ''}
+                placeholder="-- Select Office--"
+                onChange={(item) => {
+                  setData?.((cs) => ({
+                    ...cs,
+                    officeId: (item as OfficesGetResponse).id
+                  }))
+                }}
+                filterItem={(item, query) =>
+                  (item?.name
+                    ?.toLowerCase?.()
+                    ?.includes?.(query.toLowerCase()) ||
+                    item?.externalId?.toString()?.includes?.(query)) ??
+                  true
+                }
+              />
+            </FormElementComponent>
+            <FormElementComponent label="Filter">
+              <ComboBoxComponent
+                value={selectedFilter}
+                options={getDefaultOption(
+                  { key: undefined, value: '-- Select Filter--' },
+                  keyValuePairs
+                )}
+                optionLabel={(i) => i.value}
+                optionKey={(i) => i.key}
+                placeholder="-- Select Filter--"
+                onChange={(val) => {
+                  setData((cs) => ({
+                    ...cs,
+                    manualEntriesOnly: (val as { key: number; value: string })
+                      .key
+                  }))
+                }}
+              />
+            </FormElementComponent>
+
+            <div />
+
+            <FormElementComponent label="From Date">
+              <DatePickerComponent
+                placeholderText="--From--"
+                value={
+                  data?.fromDate ? new Date(data?.fromDate) : (undefined as any)
+                }
+                dateFormat="dd MMMM yyyy"
+                onChange={(val) => {
+                  setData((cs) => ({
+                    ...cs,
+                    fromDate: moment(val).format('DD MMMM YYYY')
+                  }))
+                }}
+              />
+            </FormElementComponent>
+            <FormElementComponent label="To Date">
+              <DatePickerComponent
+                placeholderText="--To--"
+                value={
+                  data?.toDate ? new Date(data?.toDate) : (undefined as any)
+                }
+                dateFormat="dd MMMM yyyy"
+                onChange={(val) => {
+                  setData((cs) => ({
+                    ...cs,
+                    toDate: moment(val).format('DD MMMM YYYY')
+                  }))
+                }}
+              />
+            </FormElementComponent>
+
+            <div />
+            <div />
+            <FormElementComponent label="Transaction ID">
+              <InputComponent
+                value={data?.transactionId ?? ''}
+                placeholder="--Transaction ID--"
+                onChange={(e) => {
+                  setData((cs) => ({
+                    ...cs,
+                    transactionId: e.target.value
+                  }))
+                }}
+              />
+            </FormElementComponent>
+            <div className="flex px-2 gap-2 justify-start ">
+              <div className="grid place-items-end ">
+                <ButtonComponent
+                  onClick={search}
+                  spinner={searchParams?.size === 0 ? false : loading}
+                >
+                  Search
+                </ButtonComponent>
+              </div>
+              <div className="grid place-items-end ">
+                <ButtonComponent
+                  className="bg-red"
+                  onClick={() => {
+                    setData({ limit: 10, offset: 0 })
+                    router.push(buildRoute(`accounting/searchtransaction`))
+                  }}
+                >
+                  Clear
+                </ButtonComponent>
+              </div>
+            </div>
+          </dl>
+          {(responseData?.pageItems?.length ?? 0) > 0 ? (
+            <div className="pt-5">
+              <TableComponent
+                loading={loading}
+                onClickRow={(r) =>
+                  router.push(
+                    buildRoute(`viewtransactions/${r[3]}`, {
+                      ...(fromDate ? { fromDate: data?.fromDate! } : {}),
+                      ...(glAccountId
+                        ? { glAccountId: data?.glAccountId! }
+                        : {}),
+                      ...(officeId ? { officeId: data?.officeId! } : {}),
+                      ...(transactionId
+                        ? { transactionId: data?.transactionId! }
+                        : {}),
+                      ...(toDate ? { toDate: data?.toDate! } : {}),
+                      ...(manualEntriesOnly
+                        ? { manualEntriesOnly: data?.manualEntriesOnly! }
+                        : {})
+                    })
+                  )
+                }
+                headings={[
+                  'Entry ID',
+                  'Office',
+                  'Transaction Date',
+                  'Transaction ID',
+                  'Type',
+                  'Created By',
+                  'Account',
+                  'Debit',
+                  'Credit'
+                ]}
+                cells={formattedData}
+              />
+              <PaginationComponent
+                count={responseData?.totalFilteredRecords! ?? 0}
+                limit={10}
+                offset={data?.offset!}
+                onChange={(_, o) => {
+                  setData((cs) => ({
+                    ...cs,
+                    offset: o
+                  }))
+                  updateQueryParams({
+                    ...data,
+                    offset: o
+                  })
+                }}
+              />
+            </div>
+          ) : null}
+        </div>
+      </CardLayout>
+    </div>
+  )
+}
+
+export default SearchJournalEntriesPage
+ */}
